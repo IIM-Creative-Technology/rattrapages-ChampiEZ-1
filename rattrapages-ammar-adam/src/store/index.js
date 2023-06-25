15ed09a88185b9c074d8168990129021b4b1c5a4
ChampiEZ <adam.ammar@edu.devinci.fr>
2023-06-25 16:27:12 +0200

    :rocket: Store set up / Q1 finished

--- rattrapages-ammar-adam/src/store/index.js
+++ rattrapages-ammar-adam/src/store/index.js
@@ -1,8 +1,39 @@
 import { createStore } from 'vuex'
 
 export default createStore({
-  state: {
-  },
+  state: { 
+    bases:[
+      { 
+        id: 1, 
+        name: 'Riz brun' 
+      },
+      { 
+        id: 2, 
+        name: 'Quinoa' 
+      },
+      { 
+        id: 3, 
+        name: 'Pâtes de blé entier' 
+      },
+    ],
+    bleuIngredients : [
+      { id: 1, name: 'Fromage bleu', category: 'Bleu' },
+      { id: 2, name: 'Noix de pécan caramélisées', category: 'Bleu' },
+      { id: 3, name: 'Raisins secs', category: 'Bleu' },
+    ],
+    vertIngredients : [
+      { id: 1, name: 'Avocat', category: 'Vert' },
+      { id: 2, name: 'Épinards frais', category: 'Vert' },
+      { id: 3, name: 'Brocoli', category: 'Vert' },
+    ],
+    drinks : [
+      { id: 1, name: 'Eau' },
+      { id: 2, name: 'Soda' },
+      { id: 3, name: 'Jus de fruits' },
+    ],
+  stock:'',
+  found:''
+},
   getters: {
   },
   mutations: {
--- rattrapages-ammar-adam/src/views/AboutView.vue
+++ rattrapages-ammar-adam/src/views/AboutView.vue
@@ -1,5 +1,37 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+
+      <div class="about">
+        <h1>Mongoo Click & Collect</h1>
+      </div>
+  <section class="body">
+    <section class="cc">
+
+    </section>
+    <aside>
+      <p>cc</p>
+    </aside>
+
+
+  </section>
 </template>
+
+
+<style scoped>
+  .body{
+    display: flex;
+  }
+
+  .cc{
+    width: 70%;
+    border: red solid 1px;
+  }
+  aside{
+    width: 30%;
+    border: blue solid 1px;
+  }
+
+  h1{
+    color: red;
+  }
+
+</style>
